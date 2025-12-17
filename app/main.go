0f1c1dcc8e76add37a87c676073509bd7b400d50
JaipreethTiruvaipati <jaipreethtsrls17@gmail.com>
2025-12-17 11:14:31 +0530

    response with 200 for response

--- app/main.go
+++ app/main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+    "fmt"
+    "net"
+    "os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+    fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+    l, err := net.Listen("tcp", "0.0.0.0:4221")
+    if err != nil {
+        fmt.Println("Failed to bind to port 4221")
+        os.Exit(1)
+    }
+    
+    // Capture the connection object ('conn') instead of discarding it with '_'
+    conn, err := l.Accept()
+    if err != nil {
+        fmt.Println("Error accepting connection: ", err.Error())
+        os.Exit(1)
+    }
+
+    // Write the HTTP response to the connection
+    // We convert the string to a byte slice because TCP transmits raw bytes
+    conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+    
+    // Close the connection to signal we are done sending data
+    conn.Close()
 }
